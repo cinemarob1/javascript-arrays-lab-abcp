da0484ece419ac7fe39055ceb1a525107c5bf712
Robert H Boscacci <cinemarob1@gmail.com>
2018-03-11 22:03:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -33,6 +33,6 @@ function prependKitten(name){
 }
 
 function removeLastKitten(name){
-  var newKittens = kittens.slice(1, kittens.length-1);
+  var newKittens = kittens.slice(0, kittens.length-1);
   return newKittens;
 }
